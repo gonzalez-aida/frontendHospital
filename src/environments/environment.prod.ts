9c946044a0e3440f21d10729c499d1dc98cd7dd6
AIDA GUADALUPE GONZALEZ GONZALEZ <aidalu13oct@gmail.com>
2026-04-09 12:24:09 -0600

    url a front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   authUrl: 'https://as-hospital.onrender.com',
   apiUrl: 'https://backhospital-khsl.onrender.com/api',
   clientId: 'hospital',
-  redirectUri: 'https://tu-frontend.onrender.com/callback',
+  redirectUri: 'https://frontendhospital.onrender.com/callback',
   appName: 'h-moscatti-frontend',
   version: '1.0.0'
 };
