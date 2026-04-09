378de23523deaa021af13444d7015241204d68ba
AIDA GUADALUPE GONZALEZ GONZALEZ <aidalu13oct@gmail.com>
2026-04-08 23:32:08 -0600

    cambios para desplegar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
-export const environment = {
+/* export const environment = {
   production: false,
   authUrl: 'http://localhost:9000',       // Auth Server
+  apiUrl: 'http://localhost:8081/api',    // MS1 Clínico
+  clientId: 'hospital',
+  redirectUri: 'http://localhost:4200/callback'
+}; */
+
+export const environment = {
+  production: false,
+  authUrl: 'https://as-hospital.onrender.com',       // Auth Server
   apiUrl: 'http://localhost:8081/api',    // MS1 Clínico
   clientId: 'hospital',
   redirectUri: 'http://localhost:4200/callback'
